4d8af2102608c76786aff61390b3624de5c72715
Arina Zaporozhets <80780314+Arina-Zap@users.noreply.github.com>
2023-05-30 13:47:38 +0200

    fixed

--- lab7-singleton/main.py
+++ lab7-singleton/main.py
@@ -2,16 +2,43 @@ class Database:
   __instance = None
 
   def __init__(self, db_type):
-    if db_type == 'pgsql':
-      #do something
+    if Database.__instance != None:
+      raise Exception("This class is a singleton!")
+    elif db_type == 'pgsql':
+      Database.__instance = PgSQL()
+      # do something
       print('This is pgsql database.')
     elif db_type == 'mongodb':
-      #do something
+      Database.__instance = MongoDB()
+      # do something
       print('This is mongodb database.')
 
   @staticmethod
   def getInstance(db_type):
-    if Database.__instance == None:
+    if Database.__instance is None:
       Database.__instance = Database(db_type)
-    return Database.__instance
+    return Database.__instance._connection
+
+
+class MongoDB:
+  _connection = None
+  def __init__(self):
+    pass
+
+  def connect(self):
+    pass
+
+
+class PgSQL:
+  _connection = None
+  def __init__(self):
+    pass
+
+  def connect(self):
+    pass
+
+
+if __name__ == "__main__":
+  pgsql_db = Database("pgsql")
+  mongo_db = Database("mongodb")
 
